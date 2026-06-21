12c2991e2a91c061baea3df445f2c9952a5156f6
Jai Prakash <jaiprakash98768@gmail.com>
2026-06-21 21:49:35 +0530

    1

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('dns').setDefaultResultOrder('ipv4first')
 const express     = require('express')
 const cors        = require('cors')
 const compression = require('compression')
